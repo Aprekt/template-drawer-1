360f999a5c5611a4861b81682a06fd14d0473575
violekdev <71582866+violekdev@users.noreply.github.com>
2022-12-10 16:20:33 +0530

    Added Drawer Animation Options

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:template_drawer_1/core/constants/enums.dart';
 import 'package:template_drawer_1/widget/side_drawer.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -10,6 +11,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<SideDrawerState> _sideDrawerKey =
+      GlobalKey<SideDrawerState>();
+  dynamic _sideDrawerState;
+  bool _isAlignmentRight = false;
+  SideDrawerType _selectedType = SideDrawerType.slide;
+
+  //Drawer Menu
   Widget buildMenu() {
     return SizedBox(
       height: 500,
@@ -36,6 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
+
+          //Drawer Menu Options
           Expanded(
             child: ListView(
               children: [
@@ -79,14 +89,35 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  final GlobalKey<SideDrawerState> _sideMenuKey = GlobalKey<SideDrawerState>();
-  dynamic _sideMenuState;
+  //Custom Radio for Drawer Animation options
+  Widget customRadioButton(String buttonName, SideDrawerType type) {
+    return Row(
+      children: [
+        Radio(
+          fillColor: MaterialStateColor.resolveWith((states) => Colors.blue),
+          value: type,
+          groupValue: _selectedType,
+          onChanged: (value) {
+            setState(() {
+              _selectedType = value!;
+            });
+          },
+        ),
+        Text(
+          buttonName,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SideDrawer(
       menu: buildMenu(),
-      key: _sideMenuKey,
+      key: _sideDrawerKey,
+      isAlignmentRight: _isAlignmentRight,
+      type: _selectedType,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter Drawer Template'),
@@ -99,33 +130,82 @@ class _MyHomePageState extends State<MyHomePage> {
           leading: IconButton(
             icon: const Icon(Icons.menu),
             onPressed: () {
-              _sideMenuState = _sideMenuKey.currentState;
-              if (_sideMenuState.isOpened) {
-                _sideMenuState.closeSideDrawer();
+              _sideDrawerState = _sideDrawerKey.currentState;
+              if (_sideDrawerState.isOpened) {
+                _sideDrawerState.closeSideDrawer();
               } else {
-                _sideMenuState.openSideDrawer();
+                _sideDrawerState.openSideDrawer();
               }
             },
           ),
         ),
         body: Container(
           color: Colors.black,
-          child: const Center(
-            child: Text.rich(
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
-              TextSpan(
-                children: [
-                  TextSpan(text: "Press "),
-                  WidgetSpan(
-                      child: Icon(Icons.menu, color: Colors.white),
-                      alignment: PlaceholderAlignment.middle),
-                  TextSpan(text: " to open side drawer")
-                ],
-                style: TextStyle(color: Colors.white),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Center(
+                child: Text.rich(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                  TextSpan(
+                    children: [
+                      TextSpan(text: "Press "),
+                      WidgetSpan(
+                          child: Icon(Icons.menu, color: Colors.white),
+                          alignment: PlaceholderAlignment.middle),
+                      TextSpan(text: " to open side drawer")
+                    ],
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
-            ),
-            // ,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Divider(height: 5, thickness: 1, color: Colors.grey),
+                  ListTile(
+                    textColor: Colors.white,
+                    title: const Text("Drawer from right"),
+                    trailing: Switch(
+                      inactiveTrackColor: Colors.grey[400],
+                      value: _isAlignmentRight,
+                      onChanged: ((value) {
+                        setState(() {
+                          _isAlignmentRight = !_isAlignmentRight;
+                        });
+                      }),
+                    ),
+                  ),
+                  const Center(
+                    child: SizedBox(
+                      width: 375,
+                      child: Divider(
+                        height: 5,
+                        thickness: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 14.0, top: 8.0),
+                    child: Text(
+                      "Drawer Animation:",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                  customRadioButton("Slide", SideDrawerType.slide),
+                  customRadioButton(
+                      'Perspective Slide', SideDrawerType.slideWithPerspective),
+                  customRadioButton(
+                      'Slide and Shrink', SideDrawerType.slideWithShrink),
+                  customRadioButton(
+                      'Slide and Rotate', SideDrawerType.slideWithRotation),
+                  customRadioButton('Slide, Shrink and Rotate',
+                      SideDrawerType.slideShrinkWithRotation),
+                ],
+              ),
+            ],
           ),
         ),
       ),
